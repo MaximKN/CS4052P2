aadeff29ec00da55450f1cf1b98df07b204bc9ed
Anna Grosul <ag235@st-andrews.ac.uk>
2016-11-22 22:38:05 +0000

    changes made to until check method

--- src/main/java/formula/pathFormula/Until.java
+++ src/main/java/formula/pathFormula/Until.java
@@ -39,7 +39,56 @@ public class Until extends PathFormula {
     //**************************** New Code ************************
 
     public boolean check(Model model){ 
-        return false;
+
+         if (this.stateFormula instanceof AtomicProp){
+            AtomicProp a = (AtomicProp) this.stateFormula;
+            State[] allStates = model.getStates(); 
+            State initial = null; 
+
+            for(State s : allStates){
+
+                if(s.isInit){
+
+                    initial = s; 
+                    break; 
+                 }
+            }
+            //checks the initial state and if it contains the correct path 
+            String[] labels = initial.getLabels)();
+            boolean contains = false; 
+            for(String l : labels){
+                if(l.equals(a.label)){
+                    contains = true; 
+                }
+            }
+            if(!contains){
+                return false; 
+            }
+
+             //checks the next states to see if they contain correct paths 
+             Trasition[] allTrans = model.getTransitions(); 
+             ArrayList<State> neighbors = new ArrayList<state>();
+             neighbors.add(initial);
+
+             // BFS breadth first search implemenation 
+            while(!neighbors.isEmpty()){
+
+                for(Transition t : allTrans){
+                    if(t.getSource().equals(neighbors.get(0).getLabel()){
+                        
+                        neighbors.add(t.getTarget);
+                     }
+                 }
+                //remove state and continue to next state
+                neighbors.remove(0);
+
+                //now check the next states to see if they contain correct labels
+                //change while to until
+             }
+         return true; 
+
+        }
+
     }
 
 
